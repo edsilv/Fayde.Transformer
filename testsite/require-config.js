a8f74697cd96ae4c4bcc44edf5ba5bd5e7a48709
edsilv <e.silverton@gmail.com>
2014-09-30 18:22:03 +0100

    moved libs to fayde.json

--- testsite/require-config.js
+++ testsite/require-config.js
@@ -2,12 +2,10 @@ var require = {
     baseUrl: "./",
     paths: {
         "text": "lib/requirejs-text/text",
-        "Fayde": "lib/Fayde/Fayde",
-        "utils": "lib/fayde.utils/Fayde.Utils",
-        "tween": "lib/tween.ts/build/tween.min"
+        "Fayde": "lib/Fayde/Fayde"
     },
-    deps: ["text", "Fayde", "utils", "tween"],
-    callback: function (text, Fayde, utils, tween) {
+    deps: ["text", "Fayde"],
+    callback: function (text, Fayde) {
         Fayde.LoadConfigJson(function (config, err) {
             if (err)
                 console.warn('Could not load fayde configuration file.', err);
@@ -18,9 +16,6 @@ var require = {
         "Fayde": {
             exports: "Fayde",
             deps: ["text"]
-        },
-        "utils": {
-            deps: ["Fayde"]
         }
     }
 };

d9f7acbecfc634f7c5a91c32506c8e58559958a8
edsilv <e.silverton@gmail.com>
2015-07-12 16:36:37 +0100

    bumped version

--- dist/fayde.transformer.js
+++ dist/fayde.transformer.js
@@ -2,7 +2,7 @@ var Fayde;
 (function (Fayde) {
     var Transformer;
     (function (Transformer) {
-        Transformer.Version = '0.7.1';
+        Transformer.Version = '0.7.2';
     })(Transformer = Fayde.Transformer || (Fayde.Transformer = {}));
 })(Fayde || (Fayde = {}));
 var ScaleTransform = Fayde.Media.ScaleTransform;
--- src/_Version.ts
+++ src/_Version.ts
@@ -1,3 +1,3 @@
 module Fayde.Transformer {
-    export var Version = '0.7.1';
+    export var Version = '0.7.2';
 }
